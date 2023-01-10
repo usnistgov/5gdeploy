ad62097d83be45aaaad4fc977f6cdab55194b6fb
Junxiao Shi <git@mail1.yoursunny.com>
2023-01-10 17:04:44 +0000

    compose: yargs

--- compose.ts
+++ compose.ts
@@ -1,6 +1,22 @@
-import getStdin from "get-stdin";
+import { promises as fs } from "node:fs";
+import path from "node:path";
+
 import { Netmask } from "netmask";
-import stdout from "stdout-stream";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const args = await yargs(hideBin(process.argv))
+  .option("cfg", {
+    demandOption: true,
+    desc: "Open5GCore cfg directory",
+    type: "string",
+  })
+  .option("out", {
+    demandOption: true,
+    desc: "Compose output directory",
+    type: "string",
+  })
+  .parseAsync();
 
 interface Compose {
   networks: Record<string, any>;
@@ -12,27 +28,30 @@ const compose: Compose = {
   services: {},
 };
 
-function buildNetwork(ip: string, cidr: string) {
-  const net = new Netmask(`${ip}/${cidr}`);
+function buildNetwork(net: string, ip: string, cidr: string) {
+  const subnet4 = new Netmask(`${ip}/${cidr}`);
   return {
     driver_opts: {
+      "com.docker.network.bridge.name": `br-${net}`,
       "com.docker.network.bridge.enable_ip_masquerade": 0,
     },
     ipam: {
       driver: "default",
       config: [{
-        subnet: net.toString(),
+        subnet: subnet4.toString(),
       }],
     },
   };
 }
+
+const CFGDIR = "/opt/phoenix/cfg/current";
 
 function buildService(ct: string) {
   const service = {
     container_name: ct,
     depends_on: {} as Record<string, unknown>, // eslint-disable-line @typescript-eslint/consistent-type-assertions
     image: "phoenix",
-    command: `/opt/phoenix/cfg/current/start.sh ${ct} 1` as string | undefined,
+    command: `${CFGDIR}/start.sh ${ct} 1` as string | undefined,
     healthcheck: undefined as unknown,
     init: true,
     cap_add: [] as string[],
@@ -45,7 +64,7 @@ function buildService(ct: string) {
 
   switch (ct) {
     case "sql": {
-      service.image = "bitnami/mariadb:10.5";
+      service.image = "bitnami/mariadb:10.9";
       service.command = undefined;
       service.healthcheck = {
         test: "mysql -u root -e 'USE smf_db; USE udm_db;'",
@@ -56,7 +75,7 @@ function buildService(ct: string) {
       };
       service.volumes.push({
         type: "bind",
-        source: "/opt/phoenix-compose/sql",
+        source: path.join(args.out, "sql"),
         target: "/docker-entrypoint-startdb.d",
         read_only: true,
       });
@@ -83,31 +102,32 @@ function buildService(ct: string) {
     service.depends_on.sql = { condition: "service_healthy" };
     service.volumes.push({
       type: "bind",
-      source: "/opt/phoenix-compose/cfg",
-      target: "/opt/phoenix/cfg/current",
+      source: path.join(args.out, "cfg"),
+      target: CFGDIR,
       read_only: true,
     });
-    service.environment.cfgdir = "/opt/phoenix/cfg/current";
+    service.environment.cfgdir = CFGDIR;
   }
   return service;
 }
 
 function addNetwork({ networks }: { networks: Record<string, any> }, net: string, ip: string) {
-  networks[net] = ip === "0.0.0.0" ? {} : {
-    ipv4_address: ip,
-  };
+  const network: { ipv4_address?: string } = {};
+  if (ip !== "0.0.0.0") {
+    network.ipv4_address = ip;
+  }
+  networks[net] = network;
 }
 
-for (let line of (await getStdin()).split("\n")) {
+for (let line of (await fs.readFile(path.join(args.cfg, "ip-map"), "utf8")).split("\n")) {
   line = line.trim();
   let tokens: [string, string, string, string];
   if (line.startsWith("#") || (tokens = line.split(/\s+/) as any).length !== 4) {
     continue;
   }
   const [ct, net, ip, cidr] = tokens;
-  compose.networks[net] ??= buildNetwork(ip, cidr);
+  compose.networks[net] ??= buildNetwork(net, ip, cidr);
   compose.services[ct] ??= buildService(ct);
   addNetwork(compose.services[ct], net, ip);
 }
-
-stdout.write(`${JSON.stringify(compose, undefined, 2)}\n`);
+await fs.writeFile(path.join(args.out, "compose.yml"), JSON.stringify(compose, undefined, 2));
