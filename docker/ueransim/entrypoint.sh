f37ec51324a9b30fe80533716d82cd4c68f59551
Junxiao Shi <git@mail1.yoursunny.com>
2023-05-11 20:50:11 +0000

    docker: UERANSIM slices and sessions

--- docker/ueransim/entrypoint.sh
+++ docker/ueransim/entrypoint.sh
@@ -41,6 +41,20 @@ ue() {
   sleep 10
   msg Generating ue.yaml
   dasel -f /UERANSIM/config/custom-ue.yaml -w json | jq '
+  def hex2number: split("") | reduce .[] as $d (0;
+    (. * 16) + ({"0":0,"1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"a":10,"A":10,"b":11,"B":11,"c":12,"C":12,"d":13,"D":13,"e":14,"E":14,"f":15,"F":15})[$d]
+  );
+
+  def to_slice: split(":") | if length == 2 then
+    { sst: (.[0] | hex2number), sd: (.[1] | hex2number) }
+  else
+    { sst: (.[0] | hex2number) }
+  end;
+
+  def to_session: split(":") | {
+    type: "IPv4", apn: .[0], slice: (.[1:] | join(":") | to_slice)
+  };
+
   .supi |= ("imsi-" + env.IMSI) |
   .mcc |= env.MCC |
   .mnc |= env.MNC |
@@ -48,6 +62,9 @@ ue() {
   .op |= (env.OP // env.OPC) |
   .opType |= (if env.OP then "OP" else "OPC" end) |
   .gnbSearchList |= (env.GNB_IPS | split(",")) |
+  .["configured-nssai"] |= (env.SLICES | split(",") | map(to_slice)) |
+  .["default-nssai"] |= (env.SLICES | split(",") | map(to_slice)) |
+  .sessions |= (env.SESSIONS | split(",") | map(to_session)) |
   .' | tee /UERANSIM/ue.yaml
 
   msg Starting 5G UE
