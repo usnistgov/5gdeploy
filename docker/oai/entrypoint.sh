d49e4a7344c4e736388b267f906418ebd2d0fea5
Junxiao Shi <git@mail1.yoursunny.com>
2023-05-16 17:09:13 +0000

    docker: unify apt-get command line

--- docker/oai/entrypoint.sh
+++ docker/oai/entrypoint.sh
@@ -14,12 +14,13 @@ gnb() {
   msg NGA netif is $GNB_NGA_IF_NAME
   msg NGU netif is $GNB_NGU_IF_NAME
 
+  sleep 10
   msg Starting 5G gNodeB
   exec /opt/oai-gnb/bin/entrypoint.sh /opt/oai-gnb/bin/nr-softmodem -O /opt/oai-gnb/etc/gnb.conf
 }
 
 nr_ue() {
-  sleep 10
+  sleep 20
   msg Starting 5G UE
   exec /opt/oai-nr-ue/bin/entrypoint.sh /opt/oai-nr-ue/bin/nr-uesoftmodem -O /opt/oai-nr-ue/etc/nr-ue.conf
 }
