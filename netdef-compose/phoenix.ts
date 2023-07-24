a77976fc7aa54d29b282f98418845a389c3ede90
Junxiao Shi <git@mail1.yoursunny.com>
2023-07-24 22:14:13 +0000

    netdef-compose: move logic into PhoenixRANBuilder

--- netdef-compose/phoenix.ts
+++ netdef-compose/phoenix.ts
@@ -27,6 +27,14 @@ export async function phoenixRAN(ctx: NetDefComposeContext): Promise<void> {
 class PhoenixScenarioBuilder {
   constructor(protected readonly ctx: NetDefComposeContext) {
     this.ctx.defineNetwork("mgmt", true);
+
+    const [mcc, mnc] = NetDef.splitPLMN(this.network.plmn);
+    assert(mnc.length === 2, "Open5GCore only supports 2-digit MNC");
+    this.sf.env.set("MCC", mcc);
+    this.sf.env.set("MNC", mnc);
+    this.sf.env.set("PROMETHEUS_ENABLED", "0");
+    this.sf.env.set("COMMAND_DISABLED", "0");
+    this.sf.env.set("DISABLE_REMOTE_COMMAND", "0");
   }
 
   public readonly sf = new ScenarioFolder();
@@ -135,7 +143,54 @@ class PhoenixCoreBuilder extends PhoenixScenarioBuilder {
   }
 
   private buildUDM(): void {
-    this.createDatabase("5g/sql/udm_db.sql");
+    const { netdef, network } = this;
+    const dfltSubscribedNSSAI = netdef.nssai.map((snssai): N.SubscriberSNSSAI => ({
+      snssai,
+      dnns: network.dataNetworks.filter((dn) => dn.snssai === snssai).map((dn) => dn.dnn),
+    }));
+
+    const db = this.createDatabase("5g/sql/udm_db.sql");
+    this.sf.appendSQL(db, function*() {
+      yield "DELETE FROM gpsi_supi_association";
+      yield "DELETE FROM supi";
+      yield "DELETE FROM gpsi";
+      yield "SELECT @am_json:=access_and_mobility_sub_data FROM am_data WHERE supi='0'";
+      yield "DELETE FROM am_data";
+      yield "SELECT @dnn_json:=json FROM dnn_configurations WHERE supi='default_data' LIMIT 1";
+      yield "DELETE FROM dnn_configurations";
+
+      for (const { supi, k, opc, subscribedNSSAI = dfltSubscribedNSSAI } of network.subscribers) {
+        yield sql`
+          INSERT supi (identity,k,amf,op,sqn,auth_type,op_is_opc,usim_type)
+          VALUES (${supi},UNHEX(${k}),UNHEX(${USIM.amf}),UNHEX(${opc}),UNHEX(${USIM.sqn}),0,1,0)
+          RETURNING @supi_id:=id
+        `;
+        yield sql`INSERT gpsi (identity) VALUES (${`msisdn-${supi}`}) RETURNING @gpsi_id:=id`;
+        yield "INSERT gpsi_supi_association (gpsi_id,supi_id) VALUES (@gpsi_id,@supi_id)";
+
+        const amPatch = {
+          nssai: {
+            defaultSingleNssais: subscribedNSSAI.map(({ snssai }) => expandSNSSAI(snssai)),
+          },
+        };
+        yield sql`INSERT am_data (supi,access_and_mobility_sub_data) VALUES (${supi},JSON_MERGE_PATCH(@am_json,${amPatch}))`;
+
+        for (const { snssai, dnns } of subscribedNSSAI) {
+          for (const dnn of dnns) {
+            const dn = netdef.findDN(dnn, snssai);
+            assert(!!dn);
+            const { sst } = expandSNSSAI(snssai);
+            const dnnPatch = {
+              pduSessionTypes: {
+                defaultSessionType: dn.type.toUpperCase(),
+              },
+            };
+            yield sql`INSERT dnn_configurations (supi,sst,dnn,json) VALUES (${supi},${sst},${dnn},JSON_MERGE_PATCH(@dnn_json,${dnnPatch}))`;
+          }
+        }
+      }
+    });
+
     this.createNetworkFunction("5g/udm.json", ["cp", "db"]);
   }
 
@@ -144,7 +199,30 @@ class PhoenixCoreBuilder extends PhoenixScenarioBuilder {
   }
 
   private buildAMFs(): void {
-    this.createNetworkFunction("5g/amf.json", ["cp", "n2"], this.ctx.network.amfs);
+    for (const [ct, amf] of this.createNetworkFunction("5g/amf.json", ["cp", "n2"], this.ctx.network.amfs)) {
+      this.sf.editNetworkFunction(ct,
+        (c) => setNrfClientSlices(c, amf.nssai ?? this.netdef.nssai),
+        (c) => {
+          const { config } = c.getModule("amf");
+          config.id = ct;
+          const [regionId, amfSetId, amfPointer] = NetDef.validateAMFI(amf.amfi);
+          config.guami = {
+            mcc: "%MCC",
+            mnc: "%MNC",
+            regionId,
+            amfSetId,
+            amfPointer,
+          };
+          config.trackingArea.splice(0, Infinity, {
+            mcc: "%MCC",
+            mnc: "%MNC",
+            taiList: [
+              { tac: this.netdef.tac },
+            ],
+          });
+        },
+      );
+    }
   }
 
   private buildSMFs(): void {
@@ -280,12 +358,84 @@ class PhoenixRANBuilder extends PhoenixScenarioBuilder {
   }
 
   private buildGNBs(): void {
-    this.createNetworkFunction("5g/gnb1.json", ["air", "n2", "n3"], this.ctx.network.gnbs);
+    const sliceKeys = ["slice", "slice2"] as const;
+    const slices = this.netdef.nssai.map((snssai) => expandSNSSAI(snssai));
+    assert(slices.length <= sliceKeys.length, `gNB allows up to ${sliceKeys.length} slices`);
+
+    for (const [ct, gnb] of this.createNetworkFunction("5g/gnb1.json", ["air", "n2", "n3"], this.ctx.network.gnbs)) {
+      this.sf.editNetworkFunction(ct, (c) => {
+        const { config } = c.getModule("gnb");
+        delete config.amf_addr;
+        delete config.amf_port;
+        config.amf_list = this.network.amfs.map((amf): PH.gnb.AMF => ({
+          ngc_addr: IPMAP.formatEnv(amf.name, "n2"),
+          ngc_sctp_port: 38412,
+        }));
+        config.mcc = "%MCC";
+        config.mnc = "%MNC";
+        ({ gnb: config.gnb_id, nci: config.cell_id } = this.netdef.splitNCI(gnb.nci));
+        config.tac = this.netdef.tac;
+
+        for (const [i, k] of sliceKeys.entries()) {
+          if (slices.length > i) {
+            config[k] = slices[i];
+          } else {
+            delete config[k]; // eslint-disable-line @typescript-eslint/no-dynamic-delete
+          }
+        }
+      });
+      this.sf.initCommands.set(ct, [
+        "iptables -I OUTPUT -p icmp --icmp-type destination-unreachable -j DROP",
+      ]);
+    }
   }
 
   private buildUEs(): void {
+    const allGNBs = this.network.gnbs.map((gnb) => gnb.name);
     this.sf.createFrom("ue-tunnel-mgmt.sh", this.tplFile("5g/ue-tunnel-mgmt.sh"));
-    this.createNetworkFunction("5g/ue1.json", ["air"], this.ctx.network.subscribers);
+    for (const [ct, subscriber] of this.createNetworkFunction("5g/ue1.json", ["air"], this.ctx.network.subscribers)) {
+      this.sf.editNetworkFunction(ct, (c) => {
+        const { config } = c.getModule("ue_5g_nas_only");
+        config.usim = {
+          supi: subscriber.supi,
+          k: subscriber.k,
+          amf: USIM.amf,
+          opc: subscriber.opc,
+          start_sqn: USIM.sqn,
+        };
+        delete config["usim-test-vector19"];
+
+        const nssai = subscriber.requestedNSSAI ?? subscriber.subscribedNSSAI ?? [];
+        config.dn_list.splice(0, Infinity, ...nssai.flatMap(({ snssai, dnns }): PH.ue_5g_nas_only.DN[] => dnns.map(
+          (dnn): PH.ue_5g_nas_only.DN => {
+            const dn = this.netdef.findDN(dnn, snssai);
+            assert(dn && dn.type !== "IPv6");
+            return {
+              dnn: dn.dnn,
+              dn_type: dn.type,
+            };
+          },
+        )));
+        config.DefaultNetwork.dnn = config.dn_list[0]?.dnn ?? "default";
+
+        config.Cell.splice(0, Infinity, ...(subscriber.gnbs ?? allGNBs).map((name): PH.ue_5g_nas_only.Cell => {
+          const ip = IPMAP.formatEnv(name, "air");
+          const gnb = this.netdef.findGNB(name);
+          assert(!!gnb);
+          return {
+            cell_id: this.netdef.splitNCI(gnb.nci).nci,
+            mcc: "%MCC",
+            mnc: "%MNC",
+            gnb_cp_addr: ip,
+            gnb_up_addr: ip,
+            gnb_port: 10000,
+          };
+        }));
+      });
+      this.sf.initCommands.set(ct, [
+        "ip link set air mtu 1470",
+      ]);
+    }
   }
 }
 
@@ -299,3 +449,5 @@ function setNrfClientSlices(c: NetworkFunction, nssai: readonly N.SNSSAI[]): voi
   const { config } = c.getModule("nrf_client");
   config.nf_profile.sNssais.splice(0, Infinity, ...nssai.map((snssai) => expandSNSSAI(snssai)));
 }
+
+const USIM = { sqn: "000000000001", amf: "8000" } as const;
--- phoenix-config/netdef.ts
+++ phoenix-config/netdef.ts
@@ -7,7 +7,6 @@ import type * as N from "../types/netdef.js";
 import type * as PH from "../types/phoenix.js";
 import type { ScenarioFolder } from "./folder.js";
 import type { IPMAP } from "./ipmap.js";
-import type { NetworkFunction } from "./nf.js";
 import { OtherTable } from "./other.js";
 
 /** Apply network definition to scenario. */
@@ -26,109 +25,11 @@ class NetDefProcessor {
 
   private readonly network: N.Network;
   private readonly ipmap: IPMAP;
-  private readonly usim = { sqn: "000000000001", amf: "8000" } as const;
 
   public process(only?: "core" | "ran"): void {
-    this.applyEnv();
-    if (!only || only === "ran") {
-      this.applyGNBs();
-      this.applyUEs();
-    }
     if (!only || only === "core") {
       this.applyNSSF();
-      this.applyAMF();
       this.applyUPF();
-      this.applyUDM();
-    }
-  }
-
-  private applyEnv(): void {
-    const [mcc, mnc] = NetDef.splitPLMN(this.network.plmn);
-    assert(mnc.length === 2, "Open5GCore only supports 2-digit MNC");
-    this.sf.env.set("MCC", mcc);
-    this.sf.env.set("MNC", mnc);
-    this.sf.env.set("PROMETHEUS_ENABLED", "0");
-    this.sf.env.set("COMMAND_DISABLED", "0");
-    this.sf.env.set("DISABLE_REMOTE_COMMAND", "0");
-  }
-
-  private applyGNBs(): void {
-    const sliceKeys = ["slice", "slice2"] as const;
-    const slices = this.netdef.nssai.map((snssai) => expandSNSSAI(snssai));
-    assert(slices.length <= sliceKeys.length, `gNB allows up to ${sliceKeys.length} slices`);
-
-    for (const [ct, gnb] of this.sf.scaleNetworkFunction("gnb1", this.network.gnbs)) {
-      this.sf.editNetworkFunction(ct, (c) => {
-        const { config } = c.getModule("gnb");
-        delete config.amf_addr;
-        delete config.amf_port;
-        config.amf_list = this.network.amfs.map((amf): PH.gnb.AMF => ({
-          ngc_addr: this.ipmap.formatEnv(amf.name, "n2"),
-          ngc_sctp_port: 38412,
-        }));
-        config.mcc = "%MCC";
-        config.mnc = "%MNC";
-        ({ gnb: config.gnb_id, nci: config.cell_id } = this.netdef.splitNCI(gnb.nci));
-        config.tac = this.netdef.tac;
-
-        for (const [i, k] of sliceKeys.entries()) {
-          if (slices.length > i) {
-            config[k] = slices[i];
-          } else {
-            delete config[k]; // eslint-disable-line @typescript-eslint/no-dynamic-delete
-          }
-        }
-      });
-      this.sf.initCommands.set(ct, [
-        "iptables -I OUTPUT -p icmp --icmp-type destination-unreachable -j DROP",
-      ]);
-    }
-  }
-
-  private applyUEs(): void {
-    const allGNBs = this.network.gnbs.map((gnb) => gnb.name);
-    for (const [ct, subscriber] of this.sf.scaleNetworkFunction(["ue1", "ue0"], this.network.subscribers)) {
-      this.sf.editNetworkFunction(ct, (c) => {
-        const { config } = c.getModule("ue_5g_nas_only");
-        config.usim = {
-          supi: subscriber.supi,
-          k: subscriber.k,
-          amf: this.usim.amf,
-          opc: subscriber.opc,
-          start_sqn: this.usim.sqn,
-        };
-        delete config["usim-test-vector19"];
-
-        const nssai = subscriber.requestedNSSAI ?? subscriber.subscribedNSSAI ?? [];
-        config.dn_list.splice(0, Infinity, ...nssai.flatMap(({ snssai, dnns }): PH.ue_5g_nas_only.DN[] => dnns.map(
-          (dnn): PH.ue_5g_nas_only.DN => {
-            const dn = this.netdef.findDN(dnn, snssai);
-            assert(dn && dn.type !== "IPv6");
-            return {
-              dnn: dn.dnn,
-              dn_type: dn.type,
-            };
-          },
-        )));
-        config.DefaultNetwork.dnn = config.dn_list[0]?.dnn ?? "default";
-
-        config.Cell.splice(0, Infinity, ...(subscriber.gnbs ?? allGNBs).map((name): PH.ue_5g_nas_only.Cell => {
-          const ip = this.ipmap.formatEnv(name, "air");
-          const gnb = this.netdef.findGNB(name);
-          assert(!!gnb);
-          return {
-            cell_id: this.netdef.splitNCI(gnb.nci).nci,
-            mcc: "%MCC",
-            mnc: "%MNC",
-            gnb_cp_addr: ip,
-            gnb_up_addr: ip,
-            gnb_port: 10000,
-          };
-        }));
-      });
-      this.sf.initCommands.set(ct, [
-        "ip link set air mtu 1470",
-      ]);
     }
   }
 
@@ -152,38 +53,6 @@ class NetDefProcessor {
         }
       }
     });
-  }
-
-  private applyAMF(): void {
-    for (const [ct, amf] of this.sf.scaleNetworkFunction(["amf", "amf1"], this.network.amfs)) {
-      this.sf.editNetworkFunction(ct,
-        (c) => this.setNrfClientSlices(c, amf.nssai),
-        (c) => {
-          const { config } = c.getModule("amf");
-          config.id = ct;
-          const [regionId, amfSetId, amfPointer] = NetDef.validateAMFI(amf.amfi);
-          config.guami = {
-            mcc: "%MCC",
-            mnc: "%MNC",
-            regionId,
-            amfSetId,
-            amfPointer,
-          };
-          config.trackingArea.splice(0, Infinity, {
-            mcc: "%MCC",
-            mnc: "%MNC",
-            taiList: [
-              { tac: this.netdef.tac },
-            ],
-          });
-        },
-      );
-    }
-  }
-
-  private setNrfClientSlices(c: NetworkFunction, nssai: readonly N.SNSSAI[] = this.netdef.nssai): void {
-    const { config } = c.getModule("nrf_client");
-    config.nf_profile.sNssais.splice(0, Infinity, ...nssai.map((snssai) => expandSNSSAI(snssai)));
   }
 
   private applyUPF(): void {
@@ -280,55 +149,6 @@ class NetDefProcessor {
         this.sf.routes.set("igw", { dest, via: this.ipmap.formatEnv(ct, "n6", "$") });
       }
     }
-  }
-
-  private applyUDM(): void {
-    const { netdef, network, usim } = this;
-    const dfltSubscribedNSSAI = netdef.nssai.map((snssai): N.SubscriberSNSSAI => ({
-      snssai,
-      dnns: network.dataNetworks.filter((dn) => dn.snssai === snssai).map((dn) => dn.dnn),
-    }));
-
-    this.sf.appendSQL("udm_db", function*() {
-      yield "DELETE FROM gpsi_supi_association";
-      yield "DELETE FROM supi";
-      yield "DELETE FROM gpsi";
-      yield "SELECT @am_json:=access_and_mobility_sub_data FROM am_data WHERE supi='0'";
-      yield "DELETE FROM am_data";
-      yield "SELECT @dnn_json:=json FROM dnn_configurations WHERE supi='default_data' LIMIT 1";
-      yield "DELETE FROM dnn_configurations";
-
-      for (const { supi, k, opc, subscribedNSSAI = dfltSubscribedNSSAI } of network.subscribers) {
-        yield sql`
-          INSERT supi (identity,k,amf,op,sqn,auth_type,op_is_opc,usim_type)
-          VALUES (${supi},UNHEX(${k}),UNHEX(${usim.amf}),UNHEX(${opc}),UNHEX(${usim.sqn}),0,1,0)
-          RETURNING @supi_id:=id
-        `;
-        yield sql`INSERT gpsi (identity) VALUES (${`msisdn-${supi}`}) RETURNING @gpsi_id:=id`;
-        yield "INSERT gpsi_supi_association (gpsi_id,supi_id) VALUES (@gpsi_id,@supi_id)";
-
-        const amPatch = {
-          nssai: {
-            defaultSingleNssais: subscribedNSSAI.map(({ snssai }) => expandSNSSAI(snssai)),
-          },
-        };
-        yield sql`INSERT am_data (supi,access_and_mobility_sub_data) VALUES (${supi},JSON_MERGE_PATCH(@am_json,${amPatch}))`;
-
-        for (const { snssai, dnns } of subscribedNSSAI) {
-          for (const dnn of dnns) {
-            const dn = netdef.findDN(dnn, snssai);
-            assert(!!dn);
-            const { sst } = expandSNSSAI(snssai);
-            const dnnPatch = {
-              pduSessionTypes: {
-                defaultSessionType: dn.type.toUpperCase(),
-              },
-            };
-            yield sql`INSERT dnn_configurations (supi,sst,dnn,json) VALUES (${supi},${sst},${dnn},JSON_MERGE_PATCH(@dnn_json,${dnnPatch}))`;
-          }
-        }
-      }
-    });
   }
 }
 
