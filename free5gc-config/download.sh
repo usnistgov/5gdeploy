92a2067e59527698fe2d894a77acf5da5cdd41c4
Junxiao Shi <git@mail1.yoursunny.com>
2023-10-25 18:15:17 +0000

    free5gc-config: honor $https_proxy in openapi-generator

--- free5gc-config/download.sh
+++ free5gc-config/download.sh
@@ -12,7 +12,13 @@ fi
 
 docker run -i --rm mikefarah/yq '.services | to_entries | .[].value.image' - <free5gc-compose/docker-compose.yaml >images.txt
 
-docker run --rm -v ./webconsole-openapi:/output openapitools/openapi-generator-cli generate \
+if [[ -n $https_proxy ]]; then
+  export _JAVA_OPTIONS="$(node -e '
+    const u = new URL(process.env.https_proxy);
+    process.stdout.write(`-Dhttps.proxyHost=${u.hostname} -Dhttps.proxyPort=${u.port}`);
+  ')"
+fi
+docker run --rm -v ./webconsole-openapi:/output -e _JAVA_OPTIONS openapitools/openapi-generator-cli generate \
   -i https://github.com/free5gc/webconsole/raw/641d0abd09fd21906d8b08fdf400fd1262c49a22/frontend/webconsole.yaml \
   -g typescript-fetch -o /output
 docker run --rm -v ./webconsole-openapi:/output alpine chown -R $(id -u):$(id -g) /output
