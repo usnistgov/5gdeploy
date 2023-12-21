4653bc53ab301ba27e0aa8bdb34b95678379b038
Junxiao Shi <git@mail1.yoursunny.com>
2023-12-21 16:09:13 +0000

    install: use 5gdeploy/install.sh

--- install.sh
+++ install.sh
@@ -13,29 +13,10 @@ die() {
   exit 1
 }
 
-for CMD in corepack docker jq; do
-  if ! command -v $CMD >/dev/null; then
-    die Missing required command $CMD
-  fi
-done
-
-if ! docker version &>/dev/null; then
-  die Unable to access Docker, check docker group membership
-fi
-
-if ! jq -e '.name == "@usnistgov/5gdeploy"' ../5gdeploy/package.json &>/dev/null; then
+if ! [[ -x ../5gdeploy/install.sh ]]; then
   die Missing 5gdeploy repository checkout
 fi
-
-msg Installing 5gdeploy
-env -C ../5gdeploy corepack pnpm install
-bash ../5gdeploy/types/build-schema.sh
-bash ../5gdeploy/free5gc/download.sh
-
-msg Building Docker images
-for IMG in bridge dn free5gc-upf ns3http phoenix ueransim; do
-  bash ../5gdeploy/docker/build.sh $IMG
-done
+../5gdeploy/install.sh
 
 msg Installing 5gdeploy-scenario
 corepack pnpm install
