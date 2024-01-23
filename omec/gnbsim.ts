174c4664fa70831c08dc7cef7eacf269b84e5823
Junxiao Shi <git@mail1.yoursunny.com>
2024-01-23 21:23:46 +0000

    omec: gNBSim, UE profiles

--- omec/gnbsim.ts
+++ omec/gnbsim.ts
@@ -1,4 +1,5 @@
-import type { PartialDeep } from "type-fest";
+import assert from "minimalistic-assert";
+import type { OptionalKeysOf, PartialDeep } from "type-fest";
 
 import * as compose from "../compose/mod.js";
 import { type N, NetDef } from "../netdef/netdef.js";
@@ -8,25 +9,29 @@ import type * as OMEC from "../types/omec.js";
 
 export async function gnbsimRAN(ctx: NetDefComposeContext): Promise<void> {
   for (const [ct, gnb] of compose.suggestNames("gnb", ctx.network.gnbs)) {
-    const s = ctx.defineService(ct, "5gdeploy.localhost/gnbsim", ["n2", "n3"]);
+    const s = ctx.defineService(ct, "5gdeploy.localhost/gnbsim", ["mgmt", "n2", "n3"]);
     const c = makeConfigUpdate(ctx, s, gnb);
-    await ctx.writeFile(`ran-cfg/${ct}.yaml`, c, { s, target: "/gnbsim/gnbsim.update.yaml" });
+    await ctx.writeFile(`ran-cfg/${ct}.yaml`, c, { s, target: "/gnbsim/config/update.yaml" });
     compose.setCommands(s, [
       "msg Preparing gNBSim config",
-      ...compose.mergeConfigFile("/gnbsim/gnbsim.update.yaml", {
-        base: "/gnbsim/gnbsim.default.yaml",
-        merged: "/gnbsim/gnbsim.yaml",
+      ...compose.mergeConfigFile("config/update.yaml", {
+        base: "config/base.yaml",
+        merged: "config/gnb.yaml",
       }),
       "sleep 10",
       "msg Starting gNBSim",
-      "exec ./gnbsim --cfg /gnbsim/gnbsim.yaml",
+      "exec bin/gnbsim --cfg config/gnb.yaml",
     ], "ash");
   }
 }
 
+function makePLMNID(network: N.Network): OMEC.PLMNID {
+  const [mcc, mnc] = NetDef.splitPLMN(network.plmn);
+  return { mcc, mnc };
+}
+
 function makeConfigUpdate(ctx: NetDefComposeContext, s: ComposeService, gnb: N.GNB): PartialDeep<OMEC.Root<OMEC.gnbsim.Configuration>> {
-  const [mcc, mnc] = NetDef.splitPLMN(ctx.network.plmn);
-  const plmnId: OMEC.PLMNID = { mcc, mnc };
+  const plmnId = makePLMNID(ctx.network);
   const nci = ctx.netdef.splitNCI(gnb.nci);
   const amfIP = ctx.gatherIPs("amf", "n2")[0]!;
   const g: OMEC.gnbsim.GNB = {
@@ -46,7 +51,7 @@ function makeConfigUpdate(ctx: NetDefComposeContext, s: ComposeService, gnb: N.G
       tac: ctx.network.tac,
       broadcastPlmnList: [{
         plmnId,
-        taiSliceSupportedList: ctx.netdef.nssai.map((snssai) => NetDef.splitSNSSAI(snssai).int),
+        taiSliceSupportList: ctx.netdef.nssai.map((snssai) => NetDef.splitSNSSAI(snssai).ih),
       }],
     }],
     defaultAmf: {
@@ -55,9 +60,61 @@ function makeConfigUpdate(ctx: NetDefComposeContext, s: ComposeService, gnb: N.G
       port: 38412,
     },
   };
+
+  const subs = ctx.netdef.listSubscribers(false).filter((sub) => sub.gnbs.includes(gnb.name));
+  const profiles: OMEC.gnbsim.Profile[] = [];
+  for (const profile of PROFILES) {
+    for (const sub of subs) {
+      profiles.push(makeProfile(ctx, gnb, sub, profile));
+    }
+  }
+
   return {
     configuration: {
       gnbs: { [gnb.name]: g },
+      httpServer: { enable: true, ipAddr: s.networks.mgmt!.ipv4_address },
+      profiles,
+      runConfigProfilesAtStart: true,
     },
+  };
+}
+
+type ProfileBase = Pick<OMEC.gnbsim.Profile, "profileType" | OptionalKeysOf<OMEC.gnbsim.Profile>>;
+
+const PROFILES: readonly ProfileBase[] = [
+  {
+    profileType: "register",
+  },
+  {
+    profileType: "pdusessest",
+    dataPktCount: 5,
+  },
+];
+
+function makeProfile(ctx: NetDefComposeContext, gnb: N.GNB, sub: NetDef.Subscriber, base: ProfileBase): OMEC.gnbsim.Profile {
+  assert(sub.requestedDN.length > 0);
+  const dn = ctx.netdef.findDN(sub.requestedDN[0]!);
+  assert(!!dn);
+  const upfNames = Array.from(ctx.netdef.listDataPathPeers(dn), ([node]) => node)
+    .filter((node): node is string => typeof node === "string");
+  assert(upfNames.length > 0);
+  const upfService = ctx.c.services[upfNames[0]!]!;
+  const plmnId = makePLMNID(ctx.network);
+
+  return {
+    profileName: `${base.profileType}-${sub.supi}`,
+    enable: true,
+    gnbName: gnb.name,
+    startImsi: sub.supi,
+    ueCount: sub.count,
+    defaultAs: upfService.networks.n6!.ipv4_address,
+    key: sub.k,
+    opc: sub.opc,
+    sequenceNumber: "000000000020",
+    dnn: dn.dnn,
+    sNssai: NetDef.splitSNSSAI(dn.snssai).ih,
+    execInParallel: false,
+    plmnId,
+    ...base,
   };
 }
--- types/omec.ts
+++ types/omec.ts
@@ -4,17 +4,19 @@ export type { Root, Info, Logger, PLMNID };
 
 export interface SNSSAI {
   sst: number;
-  sd?: number;
+  sd?: string;
 }
 
 export namespace gnbsim {
   export interface Configuration {
-    singleInterface: boolean;
-    execInParallel: boolean;
-    httpServer: unknown;
     gnbs: Record<string, GNB>;
     customProfiles: Record<string, unknown>;
-    profiles: unknown[];
+    profiles: Profile[];
+    singleInterface: boolean;
+    execInParallel: boolean;
+    httpServer: HTTPServer;
+    goProfile: unknown;
+    runConfigProfilesAtStart: boolean;
   }
 
   export interface GNB {
@@ -45,12 +47,52 @@ export namespace gnbsim {
 
   export interface BroadcastPLMN {
     plmnId: PLMNID;
-    taiSliceSupportedList: SNSSAI[];
+    taiSliceSupportList: SNSSAI[];
   }
 
   export interface AMF {
     hostName: string;
     ipAddr?: string;
     port: 38412;
+  }
+
+  export interface Profile {
+    profileType: string;
+    profileName: string;
+    enable: boolean;
+    gnbName: string;
+    startImsi: string;
+    ueCount: number;
+    plmnId: PLMNID;
+    dataPktCount?: number;
+    dataPktInterval?: number;
+    perUserTimeout?: number;
+    defaultAs: string;
+    key: string;
+    opc: string;
+    sequenceNumber: string;
+    dnn: string;
+    sNssai: SNSSAI;
+    execInParallel: boolean;
+    stepTrigger?: boolean;
+    startIteration?: string;
+    iterations?: unknown[];
+  }
+
+  export type ProfileType =
+    "register" |
+    "pdusessest" |
+    "deregister" |
+    "anrelease" |
+    "uetriggservicereq" |
+    "nwtriggeruedereg" |
+    "uereqpdusessrelease" |
+    "nwreqpdusessrelease" |
+    "custom";
+
+  export interface HTTPServer {
+    enable: boolean;
+    ipAddr: string;
+    port: string;
   }
 }
