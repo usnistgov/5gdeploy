5c37d0ce5271009e2c66398556aad06eac54c048
Junxiao Shi <git@mail1.yoursunny.com>
2024-05-28 15:15:04 +0000

    docker: dockerfilelint => hadolint

--- docker/ns3http/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-g++ -std=c++17 -Wall -o ns3http main.cpp -I/usr/include/ns3.35 \
-  $(find /usr/lib/x86_64-linux-gnu/ -name 'libns3.35-*.so' -printf ' %f' | sed -e 's/lib/-l/g' -e 's/\.so//g')
