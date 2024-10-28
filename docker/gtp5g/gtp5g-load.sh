0744ff88b04df45a3ba38a245c966d655b01343f
Junxiao Shi <git@mail1.yoursunny.com>
2024-10-28 15:54:28 +0000

    docker: simplify gtp5g image

--- docker/gtp5g/gtp5g-load.sh
+++ docker/gtp5g/gtp5g-load.sh
@@ -8,10 +8,13 @@ msg() {
 }
 
 if ! [[ -d /sys/module/gtp5g ]]; then
+  msg Decompressing gtp5g kernel module
+  cd /
+  unzip -n gtp5g.zip
   msg Loading gtp5g kernel module
-  cd /gtp5g
+  cd /gtp5g-*/
   make -j$(nproc)
   make install
 fi
 
-msg gtp5g kernel module is loaded, exiting
+msg gtp5g kernel module $(cat /sys/module/gtp5g/version) is loaded, exiting
