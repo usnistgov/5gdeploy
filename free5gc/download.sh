538cccfd68f36e5443b37883ee298a8c567f3d84
Junxiao Shi <git@mail1.yoursunny.com>
2025-02-06 19:36:07 +0000

    free5gc: run openair-generator with local input file
    Java does not work in IPv6-only server using DNS64.

--- free5gc/download.sh
+++ free5gc/download.sh
@@ -11,14 +11,8 @@ else
   git clone --branch "${TAG}" https://github.com/free5gc/free5gc-compose.git
 fi
 
-if [[ -n ${https_proxy:-} ]]; then
-  export _JAVA_OPTIONS="$(node -e '
-    const u = new URL(process.env.https_proxy);
-    process.stdout.write(`-Dhttps.proxyHost=${u.hostname} -Dhttps.proxyPort=${u.port}`);
-  ')"
-fi
-docker run --rm -v ./webconsole-openapi:/output -e _JAVA_OPTIONS openapitools/openapi-generator-cli generate \
-  -i https://github.com/free5gc/webconsole/raw/06185fd39e8176e86e73e5b7c2a4f6d0bbe07a92/frontend/webconsole.yaml \
-  -g typescript-fetch -o /output
+curl -o webconsole.yaml -fsLS https://github.com/free5gc/webconsole/raw/f4932d569dd0045fc31baca062a05d7b34e3e8e0/frontend/webconsole.yaml
+docker run --rm --network none -v ./webconsole-openapi:/output -v ./webconsole.yaml:/webconsole.yaml:ro \
+  openapitools/openapi-generator-cli generate -i /webconsole.yaml -g typescript-fetch -o /output
 docker run --rm -v ./webconsole-openapi:/output alpine:3.21 chown -R $(id -u):$(id -g) /output
 find webconsole-openapi -name '*.ts' | xargs sed -i '1 i\// @ts-nocheck'
