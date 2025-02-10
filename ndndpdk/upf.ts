b5b5e6334ebf48763643f8bdea1af83f321b335e
Junxiao Shi <git@mail1.yoursunny.com>
2025-02-10 19:52:00 +0000

    ndndpdk: N3 CIDR

--- ndndpdk/upf.ts
+++ ndndpdk/upf.ts
@@ -1,4 +1,6 @@
 
+import { Netmask } from "netmask";
+
 import { compose, type netdef, type NetDefComposeContext } from "../netdef-compose/mod.js";
 import type { ComposeService } from "../types/mod.js";
 import { assert } from "../util/mod.js";
@@ -8,10 +10,16 @@ const ndndpdkDockerImage = "localhost/ndn-dpdk";
 /** Build NDN-DPDK UPF. */
 export function ndndpdkUP(ctx: NetDefComposeContext, upf: netdef.UPF): void {
   const s = ctx.defineService(upf.name, ndndpdkDockerImage, ["mgmt", "n4", "n3", "n6"]);
-  ctx.finalize.push(() => setCommands(ctx, s, upf));
+  const n3cidr = new Netmask(ctx.c.networks.n3!.ipam.config[0]!.subnet).bitmask;
+  ctx.finalize.push(() => setCommands(ctx, s, upf, n3cidr));
 }
 
-function setCommands(ctx: NetDefComposeContext, s: ComposeService, { name: ct, peers }: netdef.UPF): void {
+function setCommands(
+    ctx: NetDefComposeContext,
+    s: ComposeService,
+    { name: ct, peers }: netdef.UPF,
+    n3cidr: number,
+): void {
   const [upfN4ip] = compose.getIPMAC(s, "n4");
   const [upfN3ip, upfN3mac] = compose.getIPMAC(s, "n3");
   const flags = [
@@ -31,17 +39,17 @@ function setCommands(ctx: NetDefComposeContext, s: ComposeService, { name: ct, p
   compose.setCommands(s, [
     ...compose.renameNetifs(s),
     "msg Waiting for NDN-DPDK ethdev",
-    `wait_ethdev() { test $(ndndpdk-ctrl list-ethdev 2>/dev/null | tee ethdev.ndjson | jq -s --arg MAC ${
+    `wait_ethdev() { test $(ndndpdk-ctrl list-ethdev 2>/dev/null | jq -s --arg MAC ${
       upfN3mac} 'map(select(.macAddr==$MAC)) | length') -gt 0; }`,
     "with_retry wait_ethdev", // `with_retry $(subshell)` is incorrect - subshell is evaluated only once
     "msg Listing NDN-DPDK ethdevs",
-    "cat ethdev.ndjson",
+    "ndndpdk-ctrl list-ethdev | tee ethdev.ndjson",
     `if [[ $(ip -o addr show to ${upfN3ip} | wc -l) -eq 0 ]]; then`,
     `  ETHDEV_ID=$(jq -r --arg MAC ${upfN3mac} 'select(.macAddr==$MAC) | .id' ethdev.ndjson | head -1)`,
     "  msg Making NDN-DPDK passthru face on $ETHDEV_ID",
     `  jq -n --arg MAC ${upfN3mac} --arg PORT $ETHDEV_ID '{scheme:"passthru",local:$MAC,port:$PORT}' | ndndpdk-ctrl create-face`,
     `  PASSTHRU_NETIF=$(ip -j link show | jq -r --arg MAC ${upfN3mac} '.[] | select(.address==$MAC and (.ifname|startswith("ndndpdkPT"))) | .ifname')`,
-    `  ip addr add ${upfN3ip}/24 dev $PASSTHRU_NETIF`,
+    `  ip addr add ${upfN3ip}/${n3cidr} dev $PASSTHRU_NETIF`,
     "  msg Listing passthru device",
     "  ip addr show dev $PASSTHRU_NETIF",
     "fi",
